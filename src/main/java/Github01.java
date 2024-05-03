284e9f90ae315942ef1ca8cb14015d3799c2d03c
Meliksah Sahin <142021374+meliksahin05@users.noreply.github.com>
2024-05-03 12:18:11 +0300

    Update Github01.java

--- src/main/java/Github01.java
+++ src/main/java/Github01.java
@@ -26,6 +26,14 @@ public class Github01 {
         System.out.println(o+p);
         System.out.println("Done codes for version 3");
 
+                //Codes version 4
+
+        int o = 12;
+        int p = 11;
+
+        System.out.println(o+p);
+        System.out.println("Done codes for version 3");
+
 
 
     }
