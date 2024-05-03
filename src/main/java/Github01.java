5ebc81e3e7f42bec5f5301df5dd891d44c87c36a
Melik <meliksahin05@gmail.com>
2024-05-03 10:22:25 +0300

    v1

---
+++ src/main/java/Github01.java
@@ -0,0 +1,15 @@
+public class Github01 {
+
+    public static void main(String[] args) {
+
+
+        //Codes version 1
+
+        int x = 5;
+        int y = 7;
+        System.out.println(x+y);
+        System.out.println("Done codes for version 1");
+
+
+    }
+}
